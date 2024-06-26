08e6d813d2828775ae671680528675e2ff589625
vitorlimac <vitor_limacavalcante@hotmail.com>
2024-06-26 13:40:31 -0300

    finish coding  terraform

--- s3-bucket-static/main.tf
+++ s3-bucket-static/main.tf
@@ -18,6 +18,24 @@ resource "aws_s3_bucket" "static_site_bucket" {
         Name = "stati_site bucket"
         enviroment = "production"
     }
-
-
 }
+
+resource "aws_s3_bucket_public_access_block" "static_site_bucket" {
+    bucket = aws_s3_bucket.static_site_bucket.id
+
+    block_public_acls       = false
+    block_public_policy     = false
+    ignore_public_acls      = false
+    restrict_public_buckts  = false
+}
+
+resource "aws_s3_bucket_acl" "static_site_bucket" {
+  depends_on = [
+    aws_s3_bucket_public_access_block.static_site_bucket,
+    aws_s3_bucket_ownership_controls.static_site_bucket,
+  ]
+
+  bucket = aws_s3_bucket.static_site_bucket.id
+  acl    = "public-read"
+}
+ 
